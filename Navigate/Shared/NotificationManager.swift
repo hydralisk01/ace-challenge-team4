5f38b584cb7651c95c68c6099c96508797c5521b
Ken Ko <ken.ko@cba.com.au>
2021-10-15 09:56:32 +1100

    Method for arrival notification, and rich notification copy

--- Navigate/Shared/NotificationManager.swift
+++ Navigate/Shared/NotificationManager.swift
@@ -55,12 +55,28 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
     func scheduleNotification() {
         let content = UNMutableNotificationContent()
         content.title = "Welcome to Eveleigh"
-        content.body = "Don't have a good day, have a great day!"
+        content.body = "Your reserved desk is now available"
         content.categoryIdentifier = Identifer.category
         content.userInfo = [:]
 
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
         let request = UNNotificationRequest(identifier: "test_local_notification", content: content, trigger: trigger)
+
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                print("Unable to Add Notification Request (\(error), \(error.localizedDescription))")
+            }
+        }
+    }
+
+    func fireArrivalNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "You have arrived at your desk"
+        content.body = "Don't have a good day, have a great day!"
+        content.userInfo = [:]
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.5, repeats: false)
+        let request = UNNotificationRequest(identifier: "fireArrivalNotification", content: content, trigger: trigger)
 
         notificationCenter.add(request) { (error) in
             if let error = error {
