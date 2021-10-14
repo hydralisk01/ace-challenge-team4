7c54faeea0b3f9e3a59b28c1051ce01af451447d
jessey1207 <44921579+jessey1207@users.noreply.github.com>
2021-10-15 01:45:44 +1100

    Add WelcomeVC

--- Navigate/Shared/ContentView.swift
+++ Navigate/Shared/ContentView.swift
@@ -5,17 +5,19 @@
 //  Created by Peter Huang on 2021/10/14.
 //
 
+import UIKit
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        WelcomeScreen()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct WelcomeScreen: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        return WelcomeViewController()
     }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
